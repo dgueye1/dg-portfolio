4d43926693a2651acc816982ed089d3c1d03611c
Doudou Sy Gueye <doudousygueye12@gmail.com>
2025-10-19 01:55:01 -0400

    revert Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 
 const Hero = ({ toggleTheme }) => (
   <header className="hero">
     <img src="/assets/headshot.jpg" alt="DG Headshot" className="hero-img" />
-    <motion.h1
-  initial={{ opacity: 0, y: 30 }}
-  animate={{ opacity: 1, y: 0 }}
-  transition={{ duration: 1 }}
-    >
-     Welcome to DG’s Blog
-    </motion.h1>
+    <h1>Hi, I’m Doudou Gueye</h1>
     <p>Front-End Developer with 3 years of experience</p>
     <button onClick={toggleTheme}>Toggle Dark/Light Mode</button>
   </header>
