36c9c0c84eb3cc6b61cf1d9613a22f6593ded427
dgueye1 <64391212+dgueye1@users.noreply.github.com>
2025-10-18 23:00:44 -0400

    Delete About.jsx

--- About.jsx
+++
@@ -1,8 +0,0 @@
-const About = () => (
-  <section className="container">
-    <h2>About Me</h2>
-    <p>I specialize in building responsive, accessible, and high-performance web apps using React, TypeScript, HTML, and CSS. I love crafting beautiful UIs and seamless user experiences.</p>
-  </section>
-);
-
-export default About;
